9a06873a8ed1f0c8a1ba5c1a4dd0bee96d6a9746
Santiago Piccinini <san@ektocomms.space>
2023-12-11 18:41:31 -0300

    Add missing xdai_bridge to defabipedia

---
+++ src/defabipedia/xdai_bridge/__init__.py
@@ -0,0 +1,19 @@
+from ..types import Chain, current_dir, ContractSpec, ContractAbi
+
+
+class EthereumContractSpecs:
+    xDaiBridge = ContractSpec(address='0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016',
+                              abi_path=current_dir() / 'xdai_bridge.json',
+                              name='xdai_bridge')
+
+
+class GnosisContractSpecs:
+    BridgeInterestReceiver = ContractSpec(address='0x670daeaF0F1a5e336090504C68179670B5059088',
+                                          abi_path=current_dir() / 'bridge_interest_receiver.json',
+                                          name='bridge_interest_receiver')
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs
+}
