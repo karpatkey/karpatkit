b19d222a2a62ac033dc592621b3d1ab4529794ac
Santiago Piccinini <san@ektocomms.space>
2023-12-12 09:26:17 -0300

    defabipedia: add lido and cowswap_signer

---
+++ src/defabipedia/cowswap_signer/__init__.py
@@ -0,0 +1,19 @@
+from ..types import Chain, current_dir, ContractSpec
+
+
+class EthereumContractSpecs:
+    CowswapSigner = ContractSpec(address='0x23dA9AdE38E4477b23770DeD512fD37b12381FAB',
+                                 abi_path=current_dir() / 'cowswap_signer.json',
+                                 name='cowswap_signer')
+
+
+class GnosisContractSpecs:
+    CowswapSigner = ContractSpec(address='0x23dA9AdE38E4477b23770DeD512fD37b12381FAB',
+                                 abi_path=current_dir() / 'cowswap_signer.json',
+                                 name='cowswap_signer')
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs
+}
---
+++ src/defabipedia/lido/__init__.py
@@ -0,0 +1,26 @@
+from ..types import Chain, current_dir, ContractSpec
+
+
+class EthereumContractSpecs:
+    stETH = ContractSpec(address='0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+                         abi_path=current_dir() / 'steth.json',
+                         name='steth')
+    wstETH = ContractSpec(address='0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
+                          abi_path=current_dir() / 'wsteth.json',
+                          name='wsteth')
+
+
+class GnosisContractSpecs:
+    # TODO not available at this point in gc
+    # stETH = ContractSpec(address='0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+    #                                     abi=load_abi('steth.json'),
+    #                                     name='steth')
+    wstETH = ContractSpec(address='0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6',
+                          abi_path=current_dir() / 'wsteth.json',
+                          name='wsteth')
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs
+}
