1ac28a662b44ec593d6826df26303aa512294f64
Mauro Bruni <mauro@ektocomms.space>
2024-04-19 20:56:52 -0300

    Add newton_mauro algorith, tests and other trials

--- src/karpatkit/nodetime.py
+++ src/karpatkit/nodetime.py
@@ -42,6 +42,8 @@ def blocks_around_time(blockchain: Chain, timestamp: float | int, iterations: in
         estimated_block_number = int(round(block.number + 2 * time_error * block_rate))
         return node.eth.get_block(estimated_block_number)
 
+    print()
+
     for it in range(iterations or max_iterations):
         if (it % 2) == 0:
             estimated_block, block_rate = linear_estimate_block(left_block, right_block)
@@ -55,6 +57,10 @@ def blocks_around_time(blockchain: Chain, timestamp: float | int, iterations: in
         else:
             return estimated_block, estimated_block
 
+        # print(f"{it=:3d}|number={estimated_block.number:10_d}|timestamp={estimated_block.timestamp:14f}|period={1/block_rate:4.1f}|")
+        print(
+            f"{it=:3d}|left={left_block.number:10_d}|right={right_block.number:10_d}|timestamp={estimated_block.timestamp:14f}|period={1/block_rate:4.1f}|"
+        )
         if (right_block.number - left_block.number) == 1:
             return left_block, right_block
 
@@ -68,3 +74,87 @@ def linear_estimation(n0, n1, t0, t1, t) -> tuple[float, float]:
     r = (n1 - n0) / (t1 - t0)
     n = r * (t - t0) + n0
     return n, r
+
+
+def newton_mauro(f, iterations, n_min, n_max):
+    """
+    Improved Newton-Raphson method for monotonic and discret functions. Search n for f(n)=0
+    """
+    n_left = n_prev = n_min
+    n_right = n = n_max
+
+    fn_prev = f(n_prev)
+    for it in range(iterations):
+        fn = f(n)
+        if it == 0 and fn * fn_prev > 0:
+            raise IndexError("No solution: f(n) for n in [n_min, n_max] isn't expected to have a cross with zero.")
+
+        if n == n_prev or abs(n - n_prev) == 1 and (fn * fn_prev) <= 0:
+            print(f"\n{it=:3d}|{n=:10_d}|{fn=:14.1f}|{dfn=:4.1f}|", end="")
+            break
+
+        if fn > 0:
+            n_right = n
+        elif fn < 0:
+            n_left = n
+        else:
+            print(f"\n{it=:3d}|{n=:10_d}|{fn=:14.1f}|{dfn=:4.1f}|", end="")
+            break
+
+        dfn = (fn - fn_prev) / (n - n_prev)
+
+        print(f"\n{it=:3d}|{n=:10_d}|{fn=:14.1f}|{dfn=:4.1f}|", end="")
+        n_prev = n
+        n = int(round(n - fn / dfn))
+        if n <= n_left:
+            n = n_left + 1
+        if n >= n_right:
+            n = n_right - 1
+        fn_prev = fn
+
+    return n
+
+
+def blocks_around_time(blockchain: Chain, timestamp: float | int, iterations: int = None, precise=True):
+    from functools import cache
+
+    node = get_node(blockchain)
+    get_block = cache(node.eth.get_block)
+    f = lambda n: get_block(n).timestamp - timestamp
+    n = newton_mauro(f, iterations or max_iterations, n_min=1, n_max=get_block("latest").number - 1)
+    b = get_block(n)
+    return (b, get_block(n + 1)) if f(n) < timestamp else (get_block(n - 1), b)
+
+
+def newton_raphson(f, iterations, n_min, n_max, moving_avg=4):
+    """
+    Search n for f(n)=0
+    """
+    n_prev = n_min
+    n = n_max
+    dfns = []
+
+    fn_prev = f(n_prev)
+    print()
+    print(f"it= -2|n={n_prev:5_d}|")
+    print(f"it= -1|{n=:10_d}|")
+    for it in range(iterations):
+        fn = f(n)
+
+        if n == n_prev or abs(n - n_prev) == 1 and (fn * fn_prev) <= 0:
+            break
+
+        dfns = [(fn - fn_prev) / (n - n_prev), *dfns[: moving_avg - 1]]
+        dfn = sum(dfns) / len(dfns)
+
+        print(f"{it=:3d}|{n=:10_d}|{fn=:14.1f}|{dfn=:4.1f}|")
+
+        n_prev = n
+        n = int(round(n - fn / dfn))
+        if n < n_min:
+            n = n_min
+        if n > n_max:
+            n = n_max
+        fn_prev = fn
+
+    return n
--- src/karpatkit/tests/test_nodetime.py
+++ src/karpatkit/tests/test_nodetime.py
@@ -1,20 +1,85 @@
+import time
+from unittest.mock import Mock
+
 import pytest
+from web3.exceptions import BlockNotFound
 
 from defabipedia import Chain
+from karpatkit import nodetime
 from karpatkit.nodetime import blocks_around_time
 
 
-@pytest.mark.parametrize("timestamp", [1_713_472_493, 1_538_269_000, 1_600_000_000])
-def test_blocks_around_time(timestamp):
-    left_block, right_block = blocks_around_time(Chain.ETHEREUM, timestamp)
-    print(
-        left_block.number,
-        right_block.number,
-        right_block.number - left_block.number,
-        left_block.timestamp,
-        right_block.timestamp,
-        right_block.timestamp - left_block.timestamp,
-    )
+@pytest.fixture(params=Chain._by_name.values())
+def blockchain(request):  # , mock_node):
+    chain = request.param
+    try:
+        nodetime.get_node(chain).eth.get_block(1)
+    except Exception as e:
+        pytest.skip(str(e))
+    else:
+        return chain
+
+
+# @pytest.fixture(params="genesis early middle late latest".split())
+@pytest.fixture(params="genesis early middle late".split())
+def timestamp(blockchain, request):
+    node = nodetime.get_node(blockchain)
+    match request.param:
+        case "genesis":
+            return node.eth.get_block(1).timestamp
+        case "early":
+            return node.eth.get_block(1).timestamp + 3600
+        case "middle":
+            return (node.eth.get_block(1).timestamp + node.eth.get_block("latest").timestamp) / 2
+        case "late":
+            return node.eth.get_block("latest").timestamp - 3600
+
+
+@pytest.fixture(params="pregenesis latest postlatest".split())
+def outside_timestamp(blockchain, request):
+    node = nodetime.get_node(blockchain)
+    match request.param:
+        case "pregenesis":
+            return node.eth.get_block(1).timestamp - 3600
+        case "latest":
+            return node.eth.get_block("latest").timestamp
+        case "postlatest":
+            return node.eth.get_block("latest").timestamp + 3600
+
+
+def test_blocks_around_time(blockchain, timestamp):
+    left_block, right_block = blocks_around_time(blockchain, timestamp)
     assert (right_block.number - left_block.number) in {0, 1}
-    assert abs(timestamp - left_block.timestamp) <= 12
-    assert abs(timestamp - right_block.timestamp) <= 12
+
+
+def test_blocks_around_time_outside(blockchain, outside_timestamp):
+    with pytest.raises(IndexError):
+        blocks_around_time(blockchain, outside_timestamp)
+
+
+@pytest.fixture
+def mock_node(monkeypatch):
+    min_number = 0
+    max_number = 19_691_386
+
+    def get_block(block_identifier):
+        if block_identifier == "latest":
+            _number = max_number
+        else:
+            _number = block_identifier
+            if block_identifier < min_number:
+                raise ValueError
+            if block_identifier > max_number:
+                raise BlockNotFound
+
+        class block:
+            number = int(_number)
+            timestamp = 1_438_269_988.0 + _number**1.01 * 12.0
+
+        return block
+
+    node = Mock()
+    node.eth.get_block = get_block
+    monkeypatch.setattr(nodetime, "get_node", Mock(return_value=node))
+    monkeypatch.setattr(nodetime, "max_iterations", 1000)
+    return node
