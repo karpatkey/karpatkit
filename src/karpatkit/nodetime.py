c928117287eef9a228d88285f3acfdc9fe04c149
Mauro Bruni <mauro@ektocomms.space>
2024-05-08 14:00:52 -0300

    Minor docstring and names changes

--- src/karpatkit/nodetime.py
+++ src/karpatkit/nodetime.py
@@ -1,26 +1,107 @@
 """
-Node pure functions to get blocks around a specific time or a block from a block number.
-It dosn't use explorers/scanners.
+Node pure functions to search blocks around a specific time. It doesn't use explorers/scanners.
 
-Them return blocks, no block numbers. But you have block.number or block.timestamp available.
+This functions return a Block object, defined here, with `block.number` and `block.timestamp`. `timestamp`s are defined
+in seconds from the Unix/POSIX epoch.
+
+The main user function is `all_chains_block_before_time`. It search all chains block in parallel. But, if you want
+to search a block just for one blockchain, you can use `block_before_time`.
 """
 import asyncio
+import logging
+from dataclasses import dataclass
+from functools import cache
 
-from defabipedia import Chain
+from defabipedia import Blockchain, Chain
 from karpatkit.node import get_node
+
+logger = logging.getLogger(__name__)
 
 max_iterations = 30
 
 
-def get_block(blockchain: Chain, block_identifier: int | str):
+@dataclass(frozen=True)
+class Block:
+    number: int
+    timestamp: float
+
+    @classmethod
+    def from_web3(cls, web3_block):
+        return cls(number=web3_block.number, timestamp=web3_block.timestamp)
+
+
+def all_chains_block_before_time(timestamp: float = None) -> dict[Blockchain, Block]:
+    """
+    Return a dict with {blockchain: block} where block is the closest block before timestamp, for all blockchains.
+    This function skips results with errors, but log them.
+    If `timestamp` isn't defined, the latest block is returned.
+    """
+    if timestamp is None:
+        async_all_chains_coro = async_all_chains_get_latest_block()
+    else:
+        async_all_chains_coro = async_all_chains_block_before_time(timestamp=timestamp)
+    d = dict()
+    for chain, block in asyncio.run(async_all_chains_coro).items():
+        if isinstance(block, Exception):
+            error = block
+            logger.error(f"For {chain}: {error!r}")
+        else:
+            d[chain] = block
+    return d
+
+
+async def async_all_chains_block_before_time(timestamp: float) -> dict[Blockchain, Block | Exception]:
+    return await async_all_chains(block_before_time, timestamp=timestamp)
+
+
+async def async_all_chains_get_latest_block() -> dict[Blockchain, Block | Exception]:
+    return await async_all_chains(get_block, block_identifier="latest")
+
+
+async def async_all_chains(func, **kwargs) -> dict[Blockchain, Block | Exception]:
+    chains = Chain.all()
+    results = await asyncio.gather(
+        *(asyncio.to_thread(func, blockchain=blockchain, **kwargs) for blockchain in chains),
+        return_exceptions=True,
+    )
+    return dict(zip(chains, results))
+
+
+def block_before_time(blockchain: Blockchain, timestamp: float) -> Block:
+    """
+    Return the closest previous for a given timestamp.
+    This uses an iterative algorithm, limited by the global `max_iterations`.
+    """
+    return blocks_around_time(blockchain, timestamp)[0]
+
+
+def blocks_around_time(blockchain: Blockchain, timestamp: float) -> tuple[Block, Block]:
+    """
+    Return two blocks, the closest previous and subsequent blocks around a given timestamp.
+    This uses an iterative algorithm, limited by the global `max_iterations`.
+    """
+
+    def f(n):
+        return cached_get_block(blockchain, n).timestamp - timestamp
+
+    block_before, block_after = discret_newton_raphson(
+        f, max_iterations, n_min=1, n_max=get_block(blockchain, "latest").number - 100
+    )
+    return cached_get_block(blockchain, block_before), cached_get_block(blockchain, block_after)
+
+
+def get_block(blockchain: Blockchain, block_identifier: int | str) -> Block:
     """
     Return just the block for a given blockchain and block_identifier, using the default node for that blockchain.
     """
     node = get_node(blockchain)
-    return node.eth.get_block(block_identifier)
+    return Block.from_web3(node.eth.get_block(block_identifier))
 
 
-def newton_mauro(f, iterations, n_min, n_max):
+cached_get_block = cache(get_block)
+
+
+def discret_newton_raphson(f, max_iterations, n_min, n_max):
     """
     Improved Newton-Raphson method for monotonic and discret functions. Search n for f(n)=0.
     The null derivative is managed better than in newton_raphson.
@@ -29,7 +110,7 @@ def newton_mauro(f, iterations, n_min, n_max):
     n_right = n = n_max
 
     fn_prev = f(n_prev)
-    for it in range(iterations):
+    for it in range(max_iterations):
         fn = f(n)
         if it == 0 and fn * fn_prev > 0:
             raise IndexError("No solution: f(n) for n in [n_min, n_max] isn't expected to have a cross with zero.")
@@ -60,124 +141,3 @@ def newton_mauro(f, iterations, n_min, n_max):
         fn_prev = fn
 
     return n_left, n_right
-
-
-def newton_raphson(f, iterations, n_min, n_max):
-    """
-    Search n for f(n)=0
-    """
-    n_prev = n_min
-    n = n_max
-
-    fn_prev = f(n_prev)
-    for it in range(iterations):
-        fn = f(n)
-        if it == 0 and fn * fn_prev > 0:
-            raise IndexError("No solution: f(n) for n in [n_min, n_max] isn't expected to have a cross with zero.")
-
-        if n == n_prev or abs(n - n_prev) == 1 and (fn * fn_prev) <= 0:
-            break
-
-        dfn = (fn - fn_prev) / (n - n_prev)
-
-        if dfn == 0:
-            break
-
-        # print(f"\n{it=:3d}|{n=:10_d}|{fn=:14.1f}|{dfn=:4.1f}|", end="")
-        n_prev = n
-        n = int(round(n - fn / dfn))
-        if n < n_min:
-            n = n_min
-        if n > n_max:
-            n = n_max
-        fn_prev = fn
-
-    return tuple(sorted([n_prev, n]))
-
-
-def blocks_around_time(
-    blockchain: Chain, timestamp: float, iterations: int = None, search_algorithm=newton_mauro, n_max=None
-):
-    """
-    Return two blocks, the closest previous and the closest before blocks around a given timestamp.
-    This is an iterative algorithm which use the `search_algorithm` estimator defined by the user.
-    The iteration are limited by defaut to `max_iterations` or the user defined `iterations`.
-    """
-    from functools import cache
-
-    node = get_node(blockchain)
-    get_block = cache(node.eth.get_block)
-
-    def f(n):
-        return get_block(n).timestamp - timestamp
-
-    nl, nr = search_algorithm(f, iterations or max_iterations, n_min=1, n_max=n_max or get_block("safe").number - 1)
-    return get_block(nl), get_block(nr)
-
-
-async def parallel_blocks_around_time(
-    timestamp: float, iterations: int = None, search_algorithm=newton_mauro, n_max=None
-):
-    """
-    Call this coroutine with `await` from within another coroutine
-
-        blocks_dict = await parallet_blocks_around_time(...)
-
-    of just from a regular funtion like
-
-        blocks_dict = asyncio.run(parallel_blocks_around_time(...))
-    """
-    chains = Chain.all()
-    results = await asyncio.gather(
-        *(
-            asyncio.to_thread(
-                blocks_around_time,
-                blockchain,
-                timestamp,
-                iterations,
-                search_algorithm,
-                n_max,
-            )
-            for blockchain in chains
-        ),
-        return_exceptions=True,
-    )
-    return ChainDict(zip(chains, results))
-
-
-class ChainDict(dict):
-    def drop_exceptions(self, exceptions=Exception):
-        return ChainDict((chain, result) for chain, result in self.items() if not isinstance(result, exceptions))
-
-    @property
-    def simple(self):
-        """
-        Return just {chain: block number} for each chain
-        """
-
-        def first(obj):
-            try:
-                (f, _) = obj
-            except ValueError:
-                return obj
-            else:
-                return f
-
-        return {chain: first(result).number for chain, result in self.items()}
-
-
-async def parallel_get_block(block_identifier: int | str):
-    chains = Chain.all()
-    results = await asyncio.gather(
-        *(asyncio.to_thread(get_block, blockchain, block_identifier) for blockchain in chains),
-        return_exceptions=True,
-    )
-    return ChainDict(zip(chains, results))
-
-
-def create_blocks_time_dict(timestamp=None, block_identifier="safe"):
-    if timestamp is None:
-        blocks_dict = asyncio.run(parallel_get_block(block_identifier))
-    else:
-        blocks_dict = asyncio.run(parallel_blocks_around_time(timestamp=timestamp))
-    return blocks_dict.drop_exceptions().simple
--- src/karpatkit/tests/test_nodetime.py
+++ src/karpatkit/tests/test_nodetime.py
@@ -1,40 +1,68 @@
-from contextlib import suppress
+import time
 
 import pytest
 
 from defabipedia import Chain
 from karpatkit import nodetime
-from karpatkit.nodetime import blocks_around_time, create_blocks_time_dict, parallel_blocks_around_time
+from karpatkit.nodetime import Block
 
-latest = dict(
-    [
-        (Chain.ETHEREUM, {"number": 19_693_336, "timestamp": 1_713_576_335}),
-        (Chain.AVALANCHE, {"number": 44_421_503, "timestamp": 1_713_576_337}),
-        (Chain.FANTOM, {"number": 79_676_149, "timestamp": 1_713_576_336}),
-        (Chain.GNOSIS, {"number": 33_531_685, "timestamp": 1_713_576_325}),
-        (Chain.ARBITRUM, {"number": 202_813_437, "timestamp": 1_713_576_341}),
-        (Chain.OPTIMISM, {"number": 118_988_778, "timestamp": 1_713_576_333}),
-        (Chain.BASE, {"number": 13_393_498, "timestamp": 1_713_576_343}),
-    ]
-)
+requested_time = 1_713_576_344
+
+expected_block_for = {
+    Chain.ETHEREUM: Block(number=19_693_336, timestamp=1_713_576_335),
+    Chain.AVALANCHE: Block(number=44_421_506, timestamp=1_713_576_343),
+    Chain.FANTOM: Block(number=79_676_154, timestamp=1_713_576_343),
+    Chain.GNOSIS: Block(number=33_531_687, timestamp=1_713_576_340),
+    Chain.ARBITRUM: Block(number=202_813_448, timestamp=1_713_576_344),
+    Chain.OPTIMISM: Block(number=118_988_778, timestamp=1_713_576_333),
+    Chain.BASE: Block(number=13_393_498, timestamp=1_713_576_343),
+}
+
+
+def test_all_chains_block_before_time():
+    r = nodetime.all_chains_block_before_time(requested_time)
+    available_chains = set(expected_block_for).intersection(r)
+    assert len(available_chains) > 0
+    for chain in available_chains:
+        if chain in {Chain.ARBITRUM}:
+            assert r[chain].timestamp == expected_block_for[chain].timestamp
+            assert abs(r[chain].timestamp - expected_block_for[chain].timestamp) <= 4
+            # because some consecutive blocks have the same timestamp
+        else:
+            assert r[chain] == expected_block_for[chain]
+
+
+def test_all_chains_block_before_time_latest():
+    now = time.time()
+    last_minute = now - 60
+    r = nodetime.all_chains_block_before_time()
+    available_chains = set(expected_block_for).intersection(r)
+    assert len(available_chains) > 0
+    for chain in available_chains:
+        assert r[chain].number > expected_block_for[chain].number
+        assert r[chain].timestamp > last_minute
+
+
+@pytest.mark.asyncio
+async def test_async_all_chains_get_latest_block():
+    r = await nodetime.async_all_chains_get_latest_block()
+    for chain in expected_block_for:
+        assert isinstance(r[chain], (Block, Exception))
+
+
+@pytest.mark.asyncio
+async def test_async_all_chains_block_before_time():
+    r = await nodetime.async_all_chains_block_before_time(requested_time)
+    for chain in expected_block_for:
+        assert isinstance(r[chain], (Block, Exception))
+
+
+## Individual tests
 
 msgs = set()
 
 
-def test_latest():
-    a, c = "{ }".split()
-    for chain in Chain.all():
-        if chain in latest:
-            continue
-        try:
-            block = nodetime.get_node(chain).eth.get_block("latest")
-        except Exception:
-            pass
-        else:
-            print(f'\n({chain}, {a}"number": {block.number}, "timestamp": {block.timestamp}{c}),')
-
-
-@pytest.fixture(params=Chain._by_name.values())
+@pytest.fixture(params=Chain.all())
 def blockchain(request):
     chain = request.param
     try:
@@ -51,8 +79,13 @@ def blockchain(request):
         return chain
 
 
+@pytest.fixture
+def latest_block(blockchain):
+    return nodetime.cached_get_block(blockchain, "latest")
+
+
 @pytest.fixture(params="genesis early middle late".split())
-def timestamp(blockchain, request, block_latest):
+def timestamp(blockchain, request, latest_block):
     node = nodetime.get_node(blockchain)
     match request.param:
         case "genesis":
@@ -60,98 +93,38 @@ def timestamp(blockchain, request, block_latest):
         case "early":
             return node.eth.get_block(1).timestamp + 3600
         case "middle":
-            return (node.eth.get_block(1).timestamp + block_latest.timestamp) / 2
+            return (node.eth.get_block(1).timestamp + latest_block.timestamp) / 2
         case "late":
-            return block_latest.timestamp - 3600
-
-
-@pytest.fixture
-def block_latest(blockchain):
-    node = nodetime.get_node(blockchain)
-    block = node.eth.get_block("latest")
-    try:
-        fixed_latest = latest[blockchain]
-    except KeyError:
-        pass
-    else:
-        block.__dict__.update(fixed_latest)
-    return block
+            return latest_block.timestamp - 3600
 
 
 @pytest.fixture(params="pregenesis postlatest".split())
-def outside_timestamp(blockchain, request, block_latest):
+def outside_timestamp(blockchain, request, latest_block):
     node = nodetime.get_node(blockchain)
     match request.param:
         case "pregenesis":
             return node.eth.get_block(1).timestamp - 3600
-        case "latest":
-            return block_latest.timestamp
         case "postlatest":
-            return block_latest.timestamp + 3600
+            return latest_block.timestamp + 3600
 
 
-def test_blocks_around_time_ok(blockchain, timestamp, block_latest):
-    left_block, right_block = blocks_around_time(blockchain, timestamp, n_max=block_latest.number - 1)
-    assert (right_block.number - left_block.number) in {0, 1}
-    assert left_block.timestamp <= timestamp
-    assert right_block.timestamp >= timestamp
+def test_blocks_around_time_ok(blockchain, timestamp):
+    block_before, block_after = nodetime.blocks_around_time(blockchain, timestamp)
+    assert (block_after.number - block_before.number) in {0, 1}
+    assert block_before.timestamp <= timestamp
+    assert block_after.timestamp >= timestamp
 
 
-def test_blocks_around_time_outside(blockchain, outside_timestamp, block_latest):
+def test_blocks_before_time_ok(blockchain):
+    block = nodetime.block_before_time(blockchain, requested_time)
+    if blockchain in [Chain.ARBITRUM]:
+        assert block.timestamp == expected_block_for[blockchain].timestamp
+        assert abs(block.timestamp - expected_block_for[blockchain].timestamp) <= 4
+        # because some consecutive blocks have the same timestamp
+    else:
+        assert block == expected_block_for[blockchain]
+
+
+def test_blocks_around_time_outside(blockchain, outside_timestamp):
     with pytest.raises(IndexError):
-        blocks_around_time(blockchain, outside_timestamp, n_max=block_latest.number - 1)
-
-
-ts = 1_713_576_335
-
-expected_simple_dict = {
-    Chain.ETHEREUM: 19_693_336,
-    Chain.AVALANCHE: 44_421_502,
-    Chain.FANTOM: 79_676_148,
-    Chain.GNOSIS: 33_531_686,
-    Chain.ARBITRUM: pytest.approx(202_813_415, abs=4),
-    Chain.BASE: 13_393_494,
-}
-
-
-@pytest.mark.asyncio
-async def test_parallel_blocks_around_time():
-    blocks_dict = await parallel_blocks_around_time(timestamp=ts)
-    print()
-    for chain, result in blocks_dict.items():
-        if isinstance(result, Exception):
-            print(chain, type(result))
-        else:
-            left, right = result
-            print(chain, left.number, right.number, left.timestamp - ts, right.timestamp - ts)
-
-    print("\nException skipped")
-    tolerance_s = 60
-    for chain, (left, right) in blocks_dict.drop_exceptions().items():
-        print(chain, left.number, right.number, left.timestamp - ts, right.timestamp - ts)
-
-        assert -tolerance_s <= (left.timestamp - ts) <= 0
-        assert 0 <= (right.timestamp - ts) <= tolerance_s
-
-        assert 0 <= (right.number - left.number) <= 1
-
-    simple_dict = blocks_dict.drop_exceptions().simple
-    for chain in expected_simple_dict:
-        with suppress(KeyError):
-            assert simple_dict[chain] == expected_simple_dict[chain]
-
-
-def test_create_blocks_time_dict():
-    simple_dict = create_blocks_time_dict(ts)
-    for chain in expected_simple_dict:
-        with suppress(KeyError):
-            assert simple_dict[chain] == expected_simple_dict[chain]
-
-
-def test_create_blocks_time_dict_latest():
-    simple_dict = create_blocks_time_dict()
-    for chain in expected_simple_dict:
-        if chain == Chain.ARBITRUM:
-            continue
-        with suppress(KeyError):
-            assert simple_dict[chain] > expected_simple_dict[chain]
+        nodetime.blocks_around_time(blockchain, outside_timestamp)
