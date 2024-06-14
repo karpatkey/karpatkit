84ab89af0afb2964330ded6175d9a2516dabb33c
ubabe53 <umberto@karpatkey.com>
2024-06-14 11:36:11 +0100

    fix: wrong naming

--- src/defabipedia/centrifuge/__init__.py
+++ src/defabipedia/centrifuge/__init__.py
@@ -2,7 +2,7 @@ from ..types import Chain, ContractSpec, current_dir
 
 
 class EthereumContractSpecs:
-    CowswapSigner = ContractSpec(
+    PoolManager = ContractSpec(
         address="0x78E9e622A57f70F1E0Ec652A4931E4e278e58142",
         abi_path=current_dir() / "pool_manager.json",
         name="pool_manager",
