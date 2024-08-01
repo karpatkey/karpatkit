9ba5946b0e7881f397e194ba16c71d4efeb13d34
ubabe53 <umberto@karpatkey.com>
2024-08-01 13:38:51 +0100

    fix: wrong chain on addresses

--- src/defabipedia/stakewisev3/__init__.py
+++ src/defabipedia/stakewisev3/__init__.py
@@ -1,14 +1,17 @@
 from ..types import Chain, ContractAbi, ContractSpec, current_dir
 
 
-class Abis:
-    OsVaultController = ContractAbi(
-        abi_path=current_dir() / "os_token_vault_controller.json", name="os_token_vault_controller"
-    )
+class GnosisAbis:
     StakingVault = ContractAbi(abi_path=current_dir() / "stake_vault.json", name="stake_vault")
 
 
-class GnosisContractSpecs:
+class EthereumAbis:
+    OsVaultController = ContractAbi(
+        abi_path=current_dir() / "os_token_vault_controller.json", name="os_token_vault_controller"
+    )
+
+
+class EthereumContractSpecs:
     OsVaultController = ContractSpec(
         address="0x2A261e60FB14586B474C208b1B7AC6D0f5000306",
         abi_path=current_dir() / "os_token_vault_controller.json",
@@ -16,6 +19,6 @@ class GnosisContractSpecs:
     )
 
 
-ContractSpecs = {Chain.GNOSIS: GnosisContractSpecs}
+ContractSpecs = {Chain.ETHEREUM: EthereumContractSpecs}
 
-Abis = {Chain.GNOSIS: Abis}
+Abis = {Chain.GNOSIS: GnosisAbis, Chain.ETHEREUM: EthereumAbis}
