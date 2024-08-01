47d538ae33f1269fd5da8821e7629a15a969b23e
ubabe53 <umberto@karpatkey.com>
2024-08-01 12:28:52 +0100

    feat: add stakewisev3 contracs and tokens

---
+++ src/defabipedia/stakewisev3/__init__.py
@@ -0,0 +1,21 @@
+from ..types import Chain, ContractAbi, ContractSpec, current_dir
+
+
+class Abis:
+    OsVaultController = ContractAbi(
+        abi_path=current_dir() / "os_token_vault_controller.json", name="os_token_vault_controller"
+    )
+    StakingVault = ContractAbi(abi_path=current_dir() / "stake_vault.json", name="stake_vault")
+
+
+class GnosisContractSpecs:
+    OsVaultController = ContractSpec(
+        address="0x2A261e60FB14586B474C208b1B7AC6D0f5000306",
+        abi_path=current_dir() / "os_token_vault_controller.json",
+        name="os_token_vault_controller",
+    )
+
+
+ContractSpecs = {Chain.GNOSIS: GnosisContractSpecs}
+
+Abis = {Chain.GNOSIS: Abis}
