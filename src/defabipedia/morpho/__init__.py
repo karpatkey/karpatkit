07c2b4f829cbd9daf2192f130ac57c6b636dd84e
memosucric <160777414+memosucric@users.noreply.github.com>
2024-10-02 22:56:36 -0300

    Update __init__.py

--- src/defabipedia/morpho/__init__.py
+++ src/defabipedia/morpho/__init__.py
@@ -12,7 +12,7 @@ class EthereumContractSpecs:
 class BaseContractSpecs:
     morpho_blue = ContractSpec(
         address="0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
-        abi_path=current_dir() / "morpho_blue.json",
+        abi_path=current_dir() / "base_morpho_blue.json",
         name="morpho_blue",
     )
 
