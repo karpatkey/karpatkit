cbe109ba46ff50b4f622db3c633f89547d4ff8ef
memosucric <160777414+memosucric@users.noreply.github.com>
2024-10-03 18:46:47 -0300

    New specs for guardians (#91)
    * Update __init__.py with Arbitrum class
    
    * Create Oracle_wstETH_USDC.json
    
    * Rename Oracle_wstETH_USDC.json to oracle_wstETH_USDC.json
    
    * Create oracle_WBTC_USDC
    
    * Create adaptative_Curve_IRM
    
    * Rename comet.json to comet_factory.json
    
    * Rename comet_rewards.json to rewards.json
    
    * Add files via upload
    
    * Update __init__.py with polygon contracts
    
    * Update and rename base_bulker.json to polygon_base_bulker.json
    
    * Update and rename cUSDCv3.json to polygon_cUSDCv3.json
    
    * Update and rename cUSDCv3_ext.json to polygon_cUSDCv3_ext.json
    
    * Update and rename cUSDTv3.json to polygon_cUSDTv3.json
    
    * Update and rename cUSDTv3_ext.json to polygon_cUSDTv3_ext.json
    
    * Update and rename comet_factory.json to polygon_comet_factory.json
    
    * Update and rename comet_proxy_admin.json to polygon_comet_proxy_admin.json
    
    * Update and rename comet_rewards.json to polygon_comet_rewards.json
    
    * Update and rename configurator.json to polygon_configurator.json
    
    * Update and rename timelock.json to polygon_timelock.json
    
    * Add files via upload
    
    * Update and rename eth_base_bulker_weth.json to eth_base_bulker.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Update __init__.py
    
    Added ethereum jsons
    
    * Add files via upload
    
    * fix bugs
    
    * test
    
    * Update and rename polygon_comet_rewards.json to polygon_rewards.json
    
    * Update and rename eth_configurator_implementation.json to eth_configurator.json
    
    * Optimism jsons
    
    * Update and rename base_bulker.json to optimism_base_bulker.json
    
    * Update and rename bridge_receiver.json to optimism_bridge_receiver.json
    
    * Update and rename cUSDCv3.json to optimism_cUSDCv3.json
    
    * Update and rename cUSDCv3_ext.json to optimism_cUSDCv3_ext.json
    
    * Update and rename cUSDTv3.json to optimism_cUSDTv3.json
    
    * Update and rename cUSDTv3_ext.json to optimism_cUSDTv3_ext.json
    
    * Update and rename cWETHv3.json to optimism_cWETHv3.json
    
    * Update and rename cWETHv3_ext.json to optimism_cWETHv3_ext.json
    
    * Update and rename comet_factory.json to optimism_comet_factory.json
    
    * Update and rename configurator.json to optimism_configurator.json
    
    * Update and rename proxy_admin.json to optimism_proxy_admin.json
    
    * Update and rename rewards.json to optimism_rewards.json
    
    * Update and rename timelock.json to optimism_timelock.json
    
    * Update and rename polygon_comet_proxy_admin.json to polygon_proxy_admin.json
    
    * Update __init__.py with optimism ContractSpecs
    
    * Add files via upload
    
    * Update and rename arb_bulker.json to arb_base_bulker.json
    
    * Update __init__.py with arbitrum ContractSpecs
    
    * Added base json files
    
    * Update __init__.py with base ContractSpecs
    
    * Add files via upload
    
    * Update adaptative_Curve_IRM
    
    * Update __init__.py with this addresses https://github.com/karpatkey/client-configs/blob/102132d5dc1d66ebf4264f12efdac16e7b208a0e/eth-sdk/addresses.ts#L259
    
    * Add files via upload
    
    * Update __init__.py with base jsons
    
    * Update rename_jsons
    
    * Code to automatically save a batch of ABIs
    
    Given a chain and list of contracts. The keys provided here are mine of a free plan for each explorer.
    
    * linting
    
    * linting
    
    * added json extension
    
    * linting
    
    ---------
    
    Co-authored-by: Richymaestro <xxxx@yyy.com>
    Co-authored-by: Richymaestro <45331793+Richymaestro@users.noreply.github.com>

--- src/defabipedia/compoundv3/__init__.py
+++ src/defabipedia/compoundv3/__init__.py
@@ -42,23 +42,23 @@ class PolygonContractSpecs:
     )
     comet_proxy_admin = ContractSpec(
         address="0xd712ACe4ca490D4F3E92992Ecf3DE12251b975F9",
-        abi_path=current_dir() / "polygon_comet_proxy_admin.json",
+        abi_path=current_dir() / "polygon_proxy_admin.json",
         name="comet_proxy_admin",
     )
     rewards = ContractSpec(
         address="0x45939657d1CA34A8FA39A924B71D28Fe8431e581",
-        abi_path=current_dir() / "polygon_comet_rewards.json",
+        abi_path=current_dir() / "polygon_rewards.json",
         name="rewards",
     )
     configurator = ContractSpec(
-        address="0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+        address="0x83E0F742cAcBE66349E3701B171eE2487a26e738",
         abi_path=current_dir() / "polygon_configurator.json",
         name="configurator",
     )
-    polygon_bridge_receiver = ContractSpec(
+    bridge_receiver = ContractSpec(
         address="0x18281dfC4d00905DA1aaA6731414EABa843c468A",
         abi_path=current_dir() / "polygon_bridge_receiver.json",
-        name="polygon_bridge_receiver",
+        name="bridge_receiver",
     )
     timelock = ContractSpec(
         address="0xCC3E7c85Bb0EE4f09380e041fee95a0caeDD4a02",
@@ -123,13 +123,8 @@ class EthereumContractSpecs:
         abi_path=current_dir() / "eth_comet_factory_WETH_USDC.json",
         name="comet_factory_WETH_USDC",
     )
-    configurator_implementation = ContractSpec(
-        address="0xcFC1fA6b7ca982176529899D99af6473aD80DF4F",
-        abi_path=current_dir() / "eth_configurator_implementation.json",
-        name="configurator_implementation",
-    )
     cwstETHv3 = ContractSpec(
-        address="0x1f0aa640e4871793ac10029365febe4e8e4b1441",
+        address="0x3D0bb1ccaB520A66e607822fC55BC921738fAFE3",
         abi_path=current_dir() / "eth_cwstETHv3.json",
         name="cwstETHv3",
     )
@@ -139,7 +134,7 @@ class EthereumContractSpecs:
         name="cwstETHv3_ext",
     )
     governor = ContractSpec(
-        address="0x6F6e4785c97885d26466945055d4Ae8931bE6f7a",
+        address="0xc0Da02939E1441F497fd74F78cE7Decb17B66529",
         abi_path=current_dir() / "eth_governor.json",
         name="governor",
     )
@@ -154,8 +149,8 @@ class EthereumContractSpecs:
         name="rewards",
     )
     configurator = ContractSpec(
-        address="0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
-        abi_path=current_dir() / "polygon_configurator.json",
+        address="0x316f9708bB98af7dA9c68C1C3b5e79039cD336E3",
+        abi_path=current_dir() / "eth_configurator.json",
         name="configurator",
     )
     timelock = ContractSpec(
@@ -165,11 +160,206 @@ class EthereumContractSpecs:
     )
 
 
-class GnosisContractSpecs:
-    pass
+class OptimismContractSpecs:
+    base_bulker = ContractSpec(
+        address="0xcb3643CC8294B23171272845473dEc49739d4Ba3",
+        abi_path=current_dir() / "optimism_base_bulker.json",
+        name="base_bulker",
+    )
+    bridge_receiver = ContractSpec(
+        address="0xC3a73A70d1577CD5B02da0bA91C0Afc8fA434DAF",
+        abi_path=current_dir() / "optimism_bridge_receiver.json",
+        name="bridge_receiver",
+    )
+    cUSDCv3 = ContractSpec(
+        address="0x2e44e174f7D53F0212823acC11C01A11d58c5bCB",
+        abi_path=current_dir() / "optimism_cUSDCv3.json",
+        name="cUSDCv3",
+    )
+    cUSDCv3_ext = ContractSpec(
+        address="0xE802a0b833f6080FEB46DD54c75444c5dba0c873",
+        abi_path=current_dir() / "optimism_cUSDCv3_ext.json",
+        name="cUSDCv3_ext",
+    )
+    cUSDTv3 = ContractSpec(
+        address="0x995E394b8B2437aC8Ce61Ee0bC610D617962B214",
+        abi_path=current_dir() / "optimism_cUSDTv3.json",
+        name="cUSDTv3",
+    )
+    cUSDTv3_ext = ContractSpec(
+        address="0xC49399814452B41dA8a7cd76a159f5515cb3e493",
+        abi_path=current_dir() / "optimism_cUSDTv3_ext.json",
+        name="cUSDTv3_ext",
+    )
+    cWETHv3 = ContractSpec(
+        address="0xE36A30D249f7761327fd973001A32010b521b6Fd",
+        abi_path=current_dir() / "optimism_cWETHv3.json",
+        name="cWETHv3",
+    )
+    cWETHv3_ext = ContractSpec(
+        address="0x82B8d9A06ccABC1e9B0c0A00f38B858E6925CF2f",
+        abi_path=current_dir() / "optimism_cWETHv3_ext.json",
+        name="cWETHv3_ext",
+    )
+    comet_factory = ContractSpec(
+        address="0xFa454dE61b317b6535A0C462267208E8FdB89f45",
+        abi_path=current_dir() / "optimism_comet_factory.json",
+        name="comet_factory",
+    )
+    configurator = ContractSpec(
+        address="0x84E93EC6170ED630f5ebD89A1AAE72d4F63f2713",
+        abi_path=current_dir() / "optimism_configurator.json",
+        name="configurator",
+    )
+    comet_proxy_admin = ContractSpec(
+        address="0x3C30B5a5A04656565686f800481580Ac4E7ed178",
+        abi_path=current_dir() / "optimism_proxy_admin.json",
+        name="comet_proxy_admin",
+    )
+    rewards = ContractSpec(
+        address="0x443EA0340cb75a160F31A440722dec7b5bc3C2E9",
+        abi_path=current_dir() / "optimism_rewards.json",
+        name="rewards",
+    )
+    timelock = ContractSpec(
+        address="0xd98Be00b5D27fc98112BdE293e487f8D4cA57d07",
+        abi_path=current_dir() / "optimism_timelock.json",
+        name="timelock",
+    )
 
 
 class ArbitrumContractSpecs:
+    base_bulker = ContractSpec(
+        address="0xbdE8F31D2DdDA895264e27DD990faB3DC87b372d",
+        abi_path=current_dir() / "arb_base_bulker.json",
+        name="base_bulker",
+    )
+    bridge_receiver = ContractSpec(
+        address="0x42480C37B249e33aABaf4c22B20235656bd38068",
+        abi_path=current_dir() / "arb_bridge_receiver.json",
+        name="bridge_receiver",
+    )
+    cUSDCev3 = ContractSpec(
+        address="0xA5EDBDD9646f8dFF606d7448e414884C7d905dCA",
+        abi_path=current_dir() / "arb_cUSDCev3.json",
+        name="cUSDCev3",
+    )
+    cUSDCv3 = ContractSpec(
+        address="0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+        abi_path=current_dir() / "arb_cUSDCv3.json",
+        name="cUSDCv3",
+    )
+    cUSDCv3_ext = ContractSpec(
+        address="0x1B2E88cC7365d90e7E81392432482925BD8437E9",
+        abi_path=current_dir() / "arb_cUSDCv3_ext.json",
+        name="cUSDCv3_ext",
+    )
+    cUSDTv3 = ContractSpec(
+        address="0xd98Be00b5D27fc98112BdE293e487f8D4cA57d07",
+        abi_path=current_dir() / "arb_cUSDTv3.json",
+        name="cUSDTv3",
+    )
+    cUSDTv3_ext = ContractSpec(
+        address="0x698A949f3b4f7a5DdE236106F25Fa0eAcA0FcEF1",
+        abi_path=current_dir() / "arb_cUSDTv3_ext.json",
+        name="cUSDTv3_ext",
+    )
+    cWETHv3 = ContractSpec(
+        address="0x6f7D514bbD4aFf3BcD1140B7344b32f063dEe486",
+        abi_path=current_dir() / "arb_cWETHv3.json",
+        name="cWETHv3",
+    )
+    cWETHv3_ext = ContractSpec(
+        address="0x5404872d8f2e24b230EC9B9eC64E3855F637FB93",
+        abi_path=current_dir() / "arb_cWETHv3_ext.json",
+        name="cWETHv3_ext",
+    )
+    comet_factory = ContractSpec(
+        address="0xe2AA5194E45B043AfdD6E98F467c0B1c13484ae9",
+        abi_path=current_dir() / "arb_comet_factory.json",
+        name="comet_factory",
+    )
+    configurator = ContractSpec(
+        address="0xb21b06D71c75973babdE35b49fFDAc3F82Ad3775",
+        abi_path=current_dir() / "arb_configurator.json",
+        name="configurator",
+    )
+    comet_proxy_admin = ContractSpec(
+        address="0xD10b40fF1D92e2267D099Da3509253D9Da4D715e",
+        abi_path=current_dir() / "arb_proxy_admin.json",
+        name="comet_proxy_admin",
+    )
+    rewards = ContractSpec(
+        address="0x88730d254A2f7e6AC8388c3198aFd694bA9f7fae",
+        abi_path=current_dir() / "arb_rewards.json",
+        name="rewards",
+    )
+    timelock = ContractSpec(
+        address="0x3fB4d38ea7EC20D91917c09591490Eeda38Cf88A",
+        abi_path=current_dir() / "arb_timelock.json",
+        name="timelock",
+    )
+
+
+class BaseContractSpecs:
+    base_bulker = ContractSpec(
+        address="0x78D0677032A35c63D142a48A2037048871212a8C",
+        abi_path=current_dir() / "base_base_bulker.json",
+        name="base_bulker",
+    )
+    bridge_receiver = ContractSpec(
+        address="0x18281dfC4d00905DA1aaA6731414EABa843c468A",
+        abi_path=current_dir() / "base_bridge_receiver.json",
+        name="bridge_receiver",
+    )
+    cUSDCv3 = ContractSpec(
+        address="0xb125E6687d4313864e53df431d5425969c15Eb2F",
+        abi_path=current_dir() / "base_cUSDCv3.json",
+        name="cUSDCv3",
+    )
+    cUSDCv3_ext = ContractSpec(
+        address="0x3bac64185786922292266AA92a58cf870D694E2a",
+        abi_path=current_dir() / "base_cUSDCv3_ext.json",
+        name="cUSDCv3_ext",
+    )
+    cUSDbCv3 = ContractSpec(
+        address="0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+        abi_path=current_dir() / "base_cUSDbCv3.json",
+        name="cUSDbCv3",
+    )
+    cUSDbCv3_ext = ContractSpec(
+        address="0x2F9E3953b2Ef89fA265f2a32ed9F80D00229125B",
+        abi_path=current_dir() / "base_cUSDbCv3_ext.json",
+        name="cUSDbCv3_ext",
+    )
+    comet_factory = ContractSpec(
+        address="0x27C348936400791b7350d80Fb81Bc61Ad68dF4AE",
+        abi_path=current_dir() / "base_comet_factory.json",
+        name="comet_factory",
+    )
+    configurator = ContractSpec(
+        address="0x45939657d1CA34A8FA39A924B71D28Fe8431e581",
+        abi_path=current_dir() / "base_configurator.json",
+        name="configurator",
+    )
+    comet_proxy_admin = ContractSpec(
+        address="0xbdE8F31D2DdDA895264e27DD990faB3DC87b372d",
+        abi_path=current_dir() / "base_proxy_admin.json",
+        name="comet_proxy_admin",
+    )
+    rewards = ContractSpec(
+        address="0x123964802e6ABabBE1Bc9547D72Ef1B69B00A6b1",
+        abi_path=current_dir() / "base_rewards.json",
+        name="rewards",
+    )
+    timelock = ContractSpec(
+        address="0xCC3E7c85Bb0EE4f09380e041fee95a0caeDD4a02",
+        abi_path=current_dir() / "base_timelock.json",
+        name="timelock",
+    )
+
+
+class GnosisContractSpecs:
     pass
 
 
@@ -178,4 +368,6 @@ ContractSpecs = {
     Chain.GNOSIS: GnosisContractSpecs,
     Chain.ARBITRUM: ArbitrumContractSpecs,
     Chain.POLYGON: PolygonContractSpecs,
+    Chain.OPTIMISM: OptimismContractSpecs,
+    Chain.BASE: BaseContractSpecs,
 }
