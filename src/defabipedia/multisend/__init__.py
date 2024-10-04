9023e69e71f9a5f2dfac4f51c731de060dfce705
Zarathustra <santiago@karpatkey.com>
2024-10-04 15:46:40 -0300

    feat_ added multisend

---
+++ src/defabipedia/multisend/__init__.py
@@ -0,0 +1,61 @@
+from defabipedia.types import Chain, ContractAbi, ContractSpec, current_dir
+
+
+class AllAbis:
+    PriceOracle = ContractAbi(abi_path=current_dir() / "price_oracle.json", name="price_oracle")
+
+
+class EthereumContractSpecs:
+    MultiSend = ContractSpec(
+        address="0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+        abi_path=current_dir() / "multisend.json",
+        name="MultiSend",
+    )
+
+class GnosisContractSpecs:
+    MultiSend = ContractSpec(
+        address="0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+        abi_path=current_dir() / "multisend.json",
+        name="MultiSend",
+    )
+
+
+class OptimismContractSpecs:
+    MultiSend = ContractSpec(
+        address="0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+        abi_path=current_dir() / "multisend.json",
+        name="MultiSend",
+    )
+
+
+class ArbitrumContractSpecs:
+    MultiSend = ContractSpec(
+        address="0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+        abi_path=current_dir() / "multisend.json",
+        name="MultiSend",
+    )
+
+
+class BaseContractSpecs:
+    MultiSend = ContractSpec(
+        address="0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+        abi_path=current_dir() / "multisend.json",
+        name="MultiSend",
+    )
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs,
+    Chain.OPTIMISM: OptimismContractSpecs,
+    Chain.ARBITRUM: ArbitrumContractSpecs,
+    Chain.BASE: BaseContractSpecs,
+}
+
+Abis = {
+    Chain.ETHEREUM: AllAbis,
+    Chain.GNOSIS: AllAbis,
+    Chain.OPTIMISM: AllAbis,
+    Chain.ARBITRUM: AllAbis,
+    Chain.BASE: AllAbis,
+}
