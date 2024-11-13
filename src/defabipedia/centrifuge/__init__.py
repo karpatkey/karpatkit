925b8da566bfe4ef1f2798989f253b58f92df0e9
umberto <55484855+ubabe53@users.noreply.github.com>
2024-11-13 16:40:15 +0100

    fix: new address for pool manager on centrifuge (#114)

--- src/defabipedia/centrifuge/__init__.py
+++ src/defabipedia/centrifuge/__init__.py
@@ -3,7 +3,7 @@ from ..types import Chain, ContractSpec, parent
 
 class EthereumContractSpecs:
     PoolManager = ContractSpec(
-        address="0x78E9e622A57f70F1E0Ec652A4931E4e278e58142",
+        address="0x91808B5E2F6d7483D41A681034D7c9DbB64B9E29",
         abi_path=parent(__file__) / "pool_manager.json",
         name="pool_manager",
     )
