a3188696926b080fed353852ec6aef414ce020b4
Richymaestro <xxxx@yyy.com>
2024-12-08 18:56:18 +0000

    lintor

--- src/defabipedia/helper_contracts/__init__.py
+++ src/defabipedia/helper_contracts/__init__.py
@@ -1,46 +1,53 @@
 from ..types import Chain, ContractAbi, ContractSpec, parent
 
+
 class EthereumContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 class GnosisContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 class OptimismContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 class ArbitrumContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 class BaseContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 class PolygonContractSpecs:
     BalanceScanner = ContractSpec(
-        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92", 
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
         abi_path=parent(__file__) / "balance_scanner.json",
-        name="balance_scanner"
+        name="balance_scanner",
     )
+
 
 ContractSpecs = {
     Chain.ETHEREUM: EthereumContractSpecs,
@@ -48,5 +55,5 @@ ContractSpecs = {
     Chain.OPTIMISM: OptimismContractSpecs,
     Chain.ARBITRUM: ArbitrumContractSpecs,
     Chain.BASE: BaseContractSpecs,
-    Chain.POLYGON: PolygonContractSpecs
+    Chain.POLYGON: PolygonContractSpecs,
 }
