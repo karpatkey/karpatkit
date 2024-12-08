3281af7f93556f11f00471dc1f1e3c074dc87e8a
Richymaestro <45331793+Richymaestro@users.noreply.github.com>
2024-12-08 21:56:07 +0000

    Balance Scanner rotki (#121)
    * Balance Scanner rotki
    
    * lintor
    
    * lintor
    
    * lintor
    
    * lintor
    
    * lintor!
    
    ---------
    
    Co-authored-by: Richymaestro <xxxx@yyy.com>

---
+++ src/defabipedia/helper_contracts/__init__.py
@@ -0,0 +1,59 @@
+from ..types import Chain, ContractSpec, parent
+
+
+class EthereumContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+class GnosisContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+class OptimismContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+class ArbitrumContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+class BaseContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+class PolygonContractSpecs:
+    BalanceScanner = ContractSpec(
+        address="0x54eCF3f6f61F63fdFE7c27Ee8A86e54899600C92",
+        abi_path=parent(__file__) / "balance_scanner.json",
+        name="balance_scanner",
+    )
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs,
+    Chain.OPTIMISM: OptimismContractSpecs,
+    Chain.ARBITRUM: ArbitrumContractSpecs,
+    Chain.BASE: BaseContractSpecs,
+    Chain.POLYGON: PolygonContractSpecs,
+}
