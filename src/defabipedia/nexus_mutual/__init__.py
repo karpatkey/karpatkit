455499eedf9f1cb65408c5cdda88cd61cfa3d5e5
ubabe53 <umberto@karpatkey.com>
2025-05-07 16:27:19 +0100

    fix: typo

--- src/defabipedia/nexus_mutual/__init__.py
+++ src/defabipedia/nexus_mutual/__init__.py
@@ -2,7 +2,7 @@ from defabipedia.types import Chain, ContractSpec, parent
 
 
 class EthereumContractSpecs:
-    oETH = ContractSpec(
+    NexusViewer = ContractSpec(
         address="0xcafeab03F219b7a8BCb92a5d61508A0AE16302b6",
         abi_path=parent(__file__) / "nexus_viewer.json",
         name="nexus_viewer",
