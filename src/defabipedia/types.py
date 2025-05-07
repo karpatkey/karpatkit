2e156cede5324cf0a44a3895a84516c8b0ef7afe
ubabe53 <umberto@karpatkey.com>
2025-05-07 16:59:19 +0100

    fix: just use a ID

--- src/defabipedia/types.py
+++ src/defabipedia/types.py
@@ -47,7 +47,7 @@ class Chain:
     KOVAN = Blockchain("kovan", 0x45)
     BASE = Blockchain("base", 0x2105)
     METIS = Blockchain("metis", 0x440)
-    COSMOS = Blockchain("cosmos", None)
+    COSMOS = Blockchain("cosmos", 0xFFFFFFFFF)
 
     # Look up indexes
     _by_id = {}
