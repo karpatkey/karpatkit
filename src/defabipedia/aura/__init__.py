d73ca205ab3a1b80a2799769de1b9605f1be3bb4
nledo <103588782+nledo@users.noreply.github.com>
2025-06-20 10:37:31 -0300

    Aura/Convex Claim Rewards Automated Actions (#157)
    Co-authored-by: Richymaestro <45331793+Richymaestro@users.noreply.github.com>

--- src/defabipedia/aura/__init__.py
+++ src/defabipedia/aura/__init__.py
@@ -2,12 +2,16 @@ from ..types import Chain, ContractAbi, ContractSpec, parent
 
 
 class AllAbis:
-    BaseRewardPool = ContractAbi(abi_path=parent(__file__) / "base_reward_pool.json", name="base_reward_pool")
+    BaseRewardPool = ContractAbi(
+        abi_path=parent(__file__) / "base_reward_pool.json", name="base_reward_pool"
+    )
 
 
 class EthereumContractSpecs:
     Booster = ContractSpec(
-        address="0xA57b8d98dAE62B26Ec3bcC4a365338157060B234", abi_path=parent(__file__) / "booster.json", name="booster"
+        address="0xA57b8d98dAE62B26Ec3bcC4a365338157060B234",
+        abi_path=parent(__file__) / "booster.json",
+        name="booster",
     )
     ClaimZap = ContractSpec(
         address="0x5b2364fD757E262253423373E4D57C5c011Ad7F4",
@@ -19,11 +23,18 @@ class EthereumContractSpecs:
         abi_path=parent(__file__) / "vl_aura.json",
         name="vl_aura",
     )
+    auraBalCompoundingRewards = ContractSpec(
+        address="0xAc16927429c5c7Af63dD75BC9d8a58c63FfD0147",
+        abi_path=parent(__file__) / "aurabal_compounding_rewards.json",
+        name="aurabal_compounding_rewards",
+    )
 
 
 class GnosisContractSpecs:
     Booster = ContractSpec(
-        address="0x98Ef32edd24e2c92525E59afc4475C1242a30184", abi_path=parent(__file__) / "booster.json", name="booster"
+        address="0x98Ef32edd24e2c92525E59afc4475C1242a30184",
+        abi_path=parent(__file__) / "booster.json",
+        name="booster",
     )
     ClaimZap = ContractSpec(
         address="0x4EA38a5739D467F7f84c06155ee2Ad745E5328E8",
@@ -32,6 +43,9 @@ class GnosisContractSpecs:
     )
 
 
-ContractSpecs = {Chain.ETHEREUM: EthereumContractSpecs, Chain.GNOSIS: GnosisContractSpecs}
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+    Chain.GNOSIS: GnosisContractSpecs,
+}
 
 Abis = {Chain.ETHEREUM: AllAbis, Chain.GNOSIS: AllAbis}
