db8151c54501bb29d36a81e3d76e480f5094fada
nledo <nicolas@karpatkey.com>
2025-06-23 17:08:02 -0300

    Convex Claim Rewards Automated Action

---
+++ src/defabipedia/convex/__init__.py
@@ -0,0 +1,22 @@
+from ..types import Chain, ContractAbi, ContractSpec, parent
+
+
+class AllAbis:
+    BaseRewardPool = ContractAbi(
+        abi_path=parent(__file__) / "base_reward_pool.json", name="base_reward_pool"
+    )
+
+
+class EthereumContractSpecs:
+    ClaimZap = ContractSpec(
+        address="0x3f29cB4111CbdA8081642DA1f75B3c12DECf2516",
+        abi_path=parent(__file__) / "claim_zap.json",
+        name="claim_zap",
+    )
+
+
+ContractSpecs = {
+    Chain.ETHEREUM: EthereumContractSpecs,
+}
+
+Abis = {Chain.ETHEREUM: AllAbis}
