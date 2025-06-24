de6450471885dfab1403a063771dc3c0dda06edc
nledo <nicolas@karpatkey.com>
2025-06-24 12:28:31 -0300

    Add Booster

--- src/defabipedia/convex/__init__.py
+++ src/defabipedia/convex/__init__.py
@@ -6,6 +6,11 @@ class AllAbis:
 
 
 class EthereumContractSpecs:
+    Booster = ContractSpec(
+        address="0xF403C135812408BFbE8713b5A23a04b3D48AAE31",
+        abi_path=parent(__file__) / "booster.json",
+        name="booster",
+    )
     ClaimZap = ContractSpec(
         address="0x3f29cB4111CbdA8081642DA1f75B3c12DECf2516",
         abi_path=parent(__file__) / "claim_zap.json",
