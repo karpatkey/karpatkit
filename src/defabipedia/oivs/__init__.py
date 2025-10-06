529026c1959c37b9d0da3841901dcae809b5edae
Richymaestro <xxxx@yyy.com>
2025-10-06 08:20:29 +0100

    shares v1 abi

--- src/defabipedia/oivs/__init__.py
+++ src/defabipedia/oivs/__init__.py
@@ -3,6 +3,7 @@ from defabipedia.types import Chain, ContractAbi, parent
 
 class AllAbis:
     nav_calculator = ContractAbi(abi_path=parent(__file__) / "nav_calculator.json", name="nav_calculator")
+    sharesv1 = ContractAbi(abi_path=parent(__file__) / "sharesv1.json", name="sharesv1")
 
 
 Abis = {
