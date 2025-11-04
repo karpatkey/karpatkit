4bd81889c09904f3c20a110834d6a654504ecc31
Richymaestro <xxxx@yyy.com>
2025-11-04 17:47:04 +0000

    new multicall, to 3

--- src/defabipedia/multicall/__init__.py
+++ src/defabipedia/multicall/__init__.py
@@ -3,33 +3,33 @@ from defabipedia.types import Chain, ContractSpec, parent
 
 class EthereumContractSpecs:
     Multicall = ContractSpec(
-        address="0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
-        abi_path=parent(__file__) / "multicall.json",
-        name="multicall",
+        address="0xcA11bde05977b3631167028862bE2a173976CA11",
+        abi_path=parent(__file__) / "multicall3.json",
+        name="multicall3",
     )
 
 
 class GnosisContractSpecs:
     Multicall = ContractSpec(
-        address="0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a",
-        abi_path=parent(__file__) / "multicall.json",
-        name="multicall",
+        address="0xcA11bde05977b3631167028862bE2a173976CA11",
+        abi_path=parent(__file__) / "multicall3.json",
+        name="multicall3",
     )
 
 
 class OptimismContractSpecs:
     Multicall = ContractSpec(
-        address="0x187C0F98FEF80E87880Db50241D40551eDd027Bf",
-        abi_path=parent(__file__) / "multicall.json",
-        name="multicall",
+        address="0xcA11bde05977b3631167028862bE2a173976CA11",
+        abi_path=parent(__file__) / "multicall3.json",
+        name="multicall3",
     )
 
 
 class ArbitrumContractSpecs:
     Multicall = ContractSpec(
-        address="0x842ec2c7d803033edf55e478f461fc547bc54eb2", #multicall2 instead of multicall but fine
-        abi_path=parent(__file__) / "multicall.json",
-        name="multicall",
+        address="0xcA11bde05977b3631167028862bE2a173976CA11", #multicall2 instead of multicall but fine
+        abi_path=parent(__file__) / "multicall3.json",
+        name="multical3",
     )
 
 
