c5bd1e9e258e9e84cab559bcc176e2232081036c
Richymaestro <xxxx@yyy.com>
2025-11-04 16:53:19 +0000

    multicalllll

--- src/defabipedia/multicall/__init__.py
+++ src/defabipedia/multicall/__init__.py
@@ -3,34 +3,34 @@ from defabipedia.types import Chain, ContractSpec, parent
 
 
 class EthereumContractSpecs:
-    MultiSend = ContractSpec(
+    Multicall = ContractSpec(
         address="0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
-        abi_path=parent(__file__) / "multisend.json",
-        name="MultiSend",
+        abi_path=parent(__file__) / "multicall.json",
+        name="multicall",
     )
 
 
 class GnosisContractSpecs:
-    MultiSend = ContractSpec(
+    Multicall = ContractSpec(
         address="0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a",
-        abi_path=parent(__file__) / "multisend.json",
-        name="MultiSend",
+        abi_path=parent(__file__) / "multicall.json",
+        name="multicall",
     )
 
 
 class OptimismContractSpecs:
-    MultiSend = ContractSpec(
+    Multicall = ContractSpec(
         address="0x187C0F98FEF80E87880Db50241D40551eDd027Bf",
-        abi_path=parent(__file__) / "multisend.json",
-        name="MultiSend",
+        abi_path=parent(__file__) / "multicall.json",
+        name="multicall",
     )
 
 
 class ArbitrumContractSpecs:
-    MultiSend = ContractSpec(
+    Multicall = ContractSpec(
         address="0x842ec2c7d803033edf55e478f461fc547bc54eb2", #multicall2 instead of multicall but fine
-        abi_path=parent(__file__) / "multisend.json",
-        name="MultiSend",
+        abi_path=parent(__file__) / "multicall.json",
+        name="multicall",
     )
 
 
