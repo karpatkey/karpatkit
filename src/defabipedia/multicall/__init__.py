5c143e065413909f2b601eec17237108ce04f79f
Richymaestro <xxxx@yyy.com>
2025-12-08 21:43:14 +0000

    multisend call only

--- src/defabipedia/multicall/__init__.py
+++ src/defabipedia/multicall/__init__.py
@@ -7,6 +7,11 @@ class EthereumContractSpecs:
         abi_path=parent(__file__) / "multicall3.json",
         name="multicall3",
     )
+    MultisendCallOnly = ContractSpec(
+        address="0xA83c336B20401Af773B6219BA5027174338D1836",
+        abi_path=parent(__file__) / "multisend_call_only.json",
+        name="multisend_call_only",
+    )
 
 
 class GnosisContractSpecs:
@@ -14,6 +19,11 @@ class GnosisContractSpecs:
         address="0xcA11bde05977b3631167028862bE2a173976CA11",
         abi_path=parent(__file__) / "multicall3.json",
         name="multicall3",
+    )
+    MultisendCallOnly = ContractSpec(
+        address="0xA83c336B20401Af773B6219BA5027174338D1836",
+        abi_path=parent(__file__) / "multisend_call_only.json",
+        name="multisend_call_only",
     )
 
 
@@ -23,6 +33,11 @@ class OptimismContractSpecs:
         abi_path=parent(__file__) / "multicall3.json",
         name="multicall3",
     )
+    MultisendCallOnly = ContractSpec(
+        address="0xA83c336B20401Af773B6219BA5027174338D1836",
+        abi_path=parent(__file__) / "multisend_call_only.json",
+        name="multisend_call_only",
+    )
 
 
 class ArbitrumContractSpecs:
@@ -31,6 +46,24 @@ class ArbitrumContractSpecs:
         abi_path=parent(__file__) / "multicall3.json",
         name="multical3",
     )
+    MultisendCallOnly = ContractSpec(
+        address="0xA83c336B20401Af773B6219BA5027174338D1836",
+        abi_path=parent(__file__) / "multisend_call_only.json",
+        name="multisend_call_only",
+    )
+
+
+class BaseContractSpecs:
+    Multicall = ContractSpec(
+        address="0xcA11bde05977b3631167028862bE2a173976CA11",  # multicall2 instead of multicall but fine
+        abi_path=parent(__file__) / "multicall3.json",
+        name="multical3",
+    )
+    MultisendCallOnly = ContractSpec(
+        address="0xA83c336B20401Af773B6219BA5027174338D1836",
+        abi_path=parent(__file__) / "multisend_call_only.json",
+        name="multisend_call_only",
+    )
 
 
 ContractSpecs = {
@@ -38,4 +71,5 @@ ContractSpecs = {
     Chain.GNOSIS: GnosisContractSpecs,
     Chain.OPTIMISM: OptimismContractSpecs,
     Chain.ARBITRUM: ArbitrumContractSpecs,
+    Chain.BASE: BaseContractSpecs,
 }
