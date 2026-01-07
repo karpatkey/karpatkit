3f6d13c1cdc44fd96c9a649c0174dcdc489e127c
Richymaestro <xxxx@yyy.com>
2026-01-07 15:56:10 +0000

    deposit and unwind

---
+++ src/defabipedia/kpk/__init__.py
@@ -0,0 +1,8 @@
+from ..types import Chain, ContractAbi, parent
+
+
+class AllAbis:
+    UnwindLeveragedPosition = ContractAbi(abi_path=parent(__file__) / "unwind_leveraged_position.json", name="unwind_leveraged_position")
+    DepositLeveragedPosition = ContractAbi(abi_path=parent(__file__) / "deposit_leveraged_position.json", name="deposit_leveraged_position")
+
+Abis = {Chain.ETHEREUM: AllAbis}
