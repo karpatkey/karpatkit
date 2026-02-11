62071afa766d34c1e33f665d1d836a36dbb8d08e
Richymaestro <xxxx@yyy.com>
2026-02-11 10:22:57 +0000

    add shares and nav contracts

--- src/defabipedia/kpk/__init__.py
+++ src/defabipedia/kpk/__init__.py
@@ -6,6 +6,14 @@ class AllAbis:
         abi_path=parent(__file__) / "aave_leveraged_position.json",
         name="AaveLeveragedPosition",
     )
+    kpkShares = ContractAbi(abi_path=parent(__file__) / "shares_v2.json", name="kpkShares")
+    NAVCalculator = ContractAbi(abi_path=parent(__file__) / "nav_calculator.json", name="NAVCalculator")
 
 
-Abis = {Chain.ETHEREUM: AllAbis}
+Abis = {
+    Chain.ETHEREUM: AllAbis,
+    Chain.GNOSIS: AllAbis,
+    Chain.OPTIMISM: AllAbis,
+    Chain.ARBITRUM: AllAbis,
+    Chain.BASE: AllAbis,
+}
